2a988beb019aa550121cda439b6df490d49cee61
charles galea <charles.galea@gmail.com>
2018-05-16 18:43:46 +0200

    initial

--- NIKON2018/Form1.Designer.cs
+++ NIKON2018/Form1.Designer.cs
@@ -29,11 +29,204 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.Xval_label = new System.Windows.Forms.Label();
+            this.Yval_label = new System.Windows.Forms.Label();
+            this.Yvalue_label = new System.Windows.Forms.Label();
+            this.Xvalue_label = new System.Windows.Forms.Label();
+            this.RESET_button = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ComboBox_Standard_Baudrates = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ComboBox_Available_SerialPorts = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 4800;
+            this.serialPort1.PortName = "COM7";
+            this.serialPort1.RtsEnable = true;
+            this.serialPort1.StopBits = System.IO.Ports.StopBits.Two;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(356, 234);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 76);
+            this.button3.TabIndex = 30;
+            this.button3.Text = "EXPORT";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Xval_label
+            // 
+            this.Xval_label.AutoSize = true;
+            this.Xval_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Xval_label.ForeColor = System.Drawing.Color.DarkGreen;
+            this.Xval_label.Location = new System.Drawing.Point(87, 318);
+            this.Xval_label.Name = "Xval_label";
+            this.Xval_label.Size = new System.Drawing.Size(55, 18);
+            this.Xval_label.TabIndex = 29;
+            this.Xval_label.Text = "X Axis";
+            // 
+            // Yval_label
+            // 
+            this.Yval_label.AutoSize = true;
+            this.Yval_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Yval_label.ForeColor = System.Drawing.Color.DarkGreen;
+            this.Yval_label.Location = new System.Drawing.Point(247, 318);
+            this.Yval_label.Name = "Yval_label";
+            this.Yval_label.Size = new System.Drawing.Size(54, 18);
+            this.Yval_label.TabIndex = 28;
+            this.Yval_label.Text = "Y Axis";
+            // 
+            // Yvalue_label
+            // 
+            this.Yvalue_label.AutoSize = true;
+            this.Yvalue_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Yvalue_label.ForeColor = System.Drawing.Color.Blue;
+            this.Yvalue_label.Location = new System.Drawing.Point(243, 336);
+            this.Yvalue_label.Name = "Yvalue_label";
+            this.Yvalue_label.Size = new System.Drawing.Size(70, 25);
+            this.Yvalue_label.TabIndex = 27;
+            this.Yvalue_label.Text = "empty";
+            // 
+            // Xvalue_label
+            // 
+            this.Xvalue_label.AutoSize = true;
+            this.Xvalue_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Xvalue_label.ForeColor = System.Drawing.Color.Blue;
+            this.Xvalue_label.Location = new System.Drawing.Point(71, 336);
+            this.Xvalue_label.Name = "Xvalue_label";
+            this.Xvalue_label.Size = new System.Drawing.Size(70, 25);
+            this.Xvalue_label.TabIndex = 24;
+            this.Xvalue_label.Text = "empty";
+            // 
+            // RESET_button
+            // 
+            this.RESET_button.Location = new System.Drawing.Point(356, 132);
+            this.RESET_button.Name = "RESET_button";
+            this.RESET_button.Size = new System.Drawing.Size(75, 76);
+            this.RESET_button.TabIndex = 23;
+            this.RESET_button.Text = "RESET";
+            this.RESET_button.UseVisualStyleBackColor = true;
+            this.RESET_button.Click += new System.EventHandler(this.RESET_button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(356, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Baudrate";
+            // 
+            // ComboBox_Standard_Baudrates
+            // 
+            this.ComboBox_Standard_Baudrates.FormattingEnabled = true;
+            this.ComboBox_Standard_Baudrates.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400"});
+            this.ComboBox_Standard_Baudrates.Location = new System.Drawing.Point(319, 22);
+            this.ComboBox_Standard_Baudrates.Name = "ComboBox_Standard_Baudrates";
+            this.ComboBox_Standard_Baudrates.Size = new System.Drawing.Size(121, 21);
+            this.ComboBox_Standard_Baudrates.TabIndex = 20;
+            this.ComboBox_Standard_Baudrates.SelectionChangeCommitted += new System.EventHandler(this.ComboBox_Standard_Baudrates_SelectionChangeCommitted);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Available Ports";
+            // 
+            // ComboBox_Available_SerialPorts
+            // 
+            this.ComboBox_Available_SerialPorts.FormattingEnabled = true;
+            this.ComboBox_Available_SerialPorts.Location = new System.Drawing.Point(111, 22);
+            this.ComboBox_Available_SerialPorts.Name = "ComboBox_Available_SerialPorts";
+            this.ComboBox_Available_SerialPorts.Size = new System.Drawing.Size(121, 21);
+            this.ComboBox_Available_SerialPorts.Sorted = true;
+            this.ComboBox_Available_SerialPorts.TabIndex = 18;
+            this.ComboBox_Available_SerialPorts.SelectionChangeCommitted += new System.EventHandler(this.ComboBox_Available_SerialPorts_SelectionChangeCommitted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(785, 575);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Xval_label);
+            this.Controls.Add(this.Yval_label);
+            this.Controls.Add(this.Yvalue_label);
+            this.Controls.Add(this.Xvalue_label);
+            this.Controls.Add(this.RESET_button);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ComboBox_Standard_Baudrates);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ComboBox_Available_SerialPorts);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "NIKON2018";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label Xval_label;
+        private System.Windows.Forms.Label Yval_label;
+        private System.Windows.Forms.Label Yvalue_label;
+        private System.Windows.Forms.Label Xvalue_label;
+        private System.Windows.Forms.Button RESET_button;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox ComboBox_Standard_Baudrates;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ComboBox_Available_SerialPorts;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- NIKON2018/Form1.cs
+++ NIKON2018/Form1.cs
@@ -7,14 +7,168 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace NIKON2018
 {
     public partial class Form1 : Form
     {
+        string Selected_Port_Baudrate;    //used for storing selected Baudrate and COMport,for displaying purposes 
+        SerialPort COMport = new SerialPort();
+        Boolean Have_message = false;
+        String sRecv;
+
+
         public Form1()
         {
             InitializeComponent();
+
+
+            //Populate the Combobox with SerialPorts on the System
+            ComboBox_Available_SerialPorts.Items.AddRange(SerialPort.GetPortNames());
+
+            // Disable Baudrate Selection also
+            ComboBox_Standard_Baudrates.Enabled = false;
+
+            COMport.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+
+        }
+
+        private void port_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            if (COMport.BytesToRead == 23) //i need only byte 113 so i use '==3'
+            {
+
+
+                sRecv = COMport.ReadExisting();
+                Have_message = true;
+                //   label1.Text = sRecv.ToString();
+            }
+
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Have_message)
+            {
+                string phrase = sRecv.ToString();
+
+
+
+                string[] words = phrase.Split(',');
+
+
+
+                String X = words[0].ToString();
+                String Y = words[1].ToString();
+
+
+
+                Xvalue_label.Text = X;
+                Yvalue_label.Text = Y;
+
+
+
+                Have_message = false;
+                Send_CMD_SXY();//lets reply SXY
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Local Variables
+            string Port_Name = ComboBox_Available_SerialPorts.SelectedItem.ToString();    // Store the selected COM port name to "Port_Name" varaiable
+            int Baud_Rate = Convert.ToInt32(ComboBox_Standard_Baudrates.SelectedItem); // Convert the string "9600" to int32 9600
+                                                                                       //Store the string in Textbox to variable "Data"
+
+            //SerialPort COMport = new SerialPort(Port_Name, Baud_Rate, Parity.None, 8, StopBits.Two); //Create a new  SerialPort Object (defaullt setting -> 8N1)
+            COMport.Encoding = Encoding.ASCII;
+            COMport.PortName = Port_Name;
+            COMport.BaudRate = Baud_Rate;
+            COMport.Parity = Parity.None;
+            COMport.DataBits = 8;
+            COMport.StopBits = StopBits.Two;
+            COMport.DtrEnable = true;
+            COMport.RtsEnable = true;
+
+            try
+            {
+
+                COMport.Open();
+            }
+            #region  
+            catch (UnauthorizedAccessException SerialException) //exception that is thrown when the operating system denies access 
+            {
+                MessageBox.Show(SerialException.ToString());
+
+                COMport.Close();
+            }
+
+            catch (System.IO.IOException SerialException)     // An attempt to set the state of the underlying port failed
+            {
+                MessageBox.Show(SerialException.ToString());
+
+                COMport.Close();
+            }
+
+            catch (InvalidOperationException SerialException) // The specified port on the current instance of the SerialPort is already open
+            {
+                MessageBox.Show(SerialException.ToString());
+
+                COMport.Close();
+            }
+
+            catch //Any other ERROR
+            {
+                MessageBox.Show("ERROR in Opening Serial PORT -- UnKnown ERROR");
+                COMport.Close();
+            }
+            #endregion
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            COMport.Close();
+        }
+
+        private void RESET_button_Click(object sender, EventArgs e)
+        {
+            Send_CMD_SXY();
+        }
+
+        private void Send_CMD_SXY()
+        {
+            if (COMport.IsOpen == true)
+            {
+                String testStr = "SXY\r\n"; // Ascii "53 58 59 0D 0A"
+                byte[] buf = System.Text.Encoding.UTF8.GetBytes(testStr);
+                COMport.Write(buf, 0, buf.Length);
+            }
+
+            else
+            {
+                //Do nothng
+            }
+        }
+
+        private void ComboBox_Standard_Baudrates_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            Selected_Port_Baudrate = Selected_Port_Baudrate + Environment.NewLine;
+            Selected_Port_Baudrate = Selected_Port_Baudrate + ComboBox_Standard_Baudrates.SelectedItem.ToString() + " bps selected";
+        }
+
+
+        private void ComboBox_Available_SerialPorts_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            Selected_Port_Baudrate = ComboBox_Available_SerialPorts.SelectedItem.ToString() + " Selected"; // Store the Selected COM port
+
+                          
+
+            ComboBox_Standard_Baudrates.Enabled = true;       // Enable Baudrate Selection after COM port is Selected
         }
     }
 }
